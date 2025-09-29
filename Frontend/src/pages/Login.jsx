758adca3ab3c56fffe50804688b559bced355e60
AsadArmanKhan <asadarmankhanworkmail@gmail.com>
2025-09-29 12:05:24 +0530

    Login Page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -6,7 +6,23 @@ export default function Login() {
 
   return (
     <>
-      <div className=""></div>
+      <div className="flex">
+        <div className="w-full md:w-1/2 flex-col justify-center items-center p-8 md:p-12">
+          <form className="w-full max-w-md bg-white p-8 rounded-lg border shadow-sm">
+            <div className="flex justify-center mb-6">
+              <h2 className="text-xl font-medium">Rabbit</h2>
+            </div>
+            <h2 className=" text-2xl font-bold text-center mb-6">Hey there!</h2>
+            <p className=" text-center mb-6">
+              {" "}
+              Enter your username and password to login
+            </p>
+            <div className=" mb-4">
+              <label className=" block text-sm font-semibold mb-2"></label>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
