40a463ea4143e0b9933ea0cf80f3f3411424d5b1
Asad Arman Khan <asadarmankhanworkmail@gmail.com>
2026-07-06 13:08:57 +0530

    ProductDetails Page Fixes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import CollectionPage from "./pages/CollectionPage";
+import ProductDetails from "./components/Products/ProductDetails";
 export default function App() {
   return (
     <>
@@ -24,6 +25,7 @@ export default function App() {
               path="collections/:collection"
               element={<CollectionPage />}
             />
+            <Route path="product/:id" element={<ProductDetails />} />
           </Route>
           <Route>{/* Admin Layout */} </Route>
         </Routes>
