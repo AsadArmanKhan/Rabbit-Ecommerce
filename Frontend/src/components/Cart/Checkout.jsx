02ffb604e891b04b860e27b4db613eb9eeaca582
Asad Arman Khan <asadarmankhanworkmail@gmail.com>
2026-07-08 12:33:44 +0530

    Checkout Page building

--- Frontend/src/components/Cart/Checkout.jsx
+++ Frontend/src/components/Cart/Checkout.jsx
@@ -38,5 +38,26 @@ export default function Checkout() {
     country: "",
     phone: "",
   });
-  return <div>Checkout</div>;
+  return (
+    <div className="border grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter">
+      {/* Left Section */}
+      <div className="border bg-white rounded-lg p-6">
+        <h2 className="text-2xl uppercase mb-6">Checkout</h2>
+        <form>
+          <h3 className="text-lg mb-4">Contact Details</h3>
+          <div className="mb-4">
+            <label className="block text-gray-700">Email</label>
+            <input
+              type="email"
+              value="user@example.com"
+              className="w-full p-2 border rounded"
+              disabled
+            />
+          </div>
+          <h3 className="text-lg mb-4">Delivery</h3>
+          <div className=""></div>
+        </form>
+      </div>
+    </div>
+  );
 }
